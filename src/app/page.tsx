e10d010772d786da1147bed058391273db98776b
pinsoegustave <ntwalimudasubirag@gmail.com>
2024-07-13 23:13:11 +0200

    Added components on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 import Cart from "@/components/frontend/Cart";
+import Feature from "@/components/frontend/Feature";
 import Hero from "@/components/frontend/Hero";
 import Navbar from "@/components/frontend/Navbar";
 import React, { useState } from "react";
@@ -15,6 +16,7 @@ export default function Home() {
       <Navbar setShowCart={setShowCart} />
       { showCart && <Cart setShowCart={setShowCart} />}
       <Hero />
+      <Feature />
    </main>
   );
 }
--- src/components/frontend/Navbar.tsx
+++ src/components/frontend/Navbar.tsx
@@ -14,7 +14,7 @@ const Navbar = ({ setShowCart }: PropsType) => {
     <div className="pt-4 bg-white top-0 sticky">
       <div className="container">
         <div className="flex justify-between items-center">
-          <div className="text-4xl font-bold">Jersey Commerce</div>
+          <div className="text-4xl font-bold">Jersey Pinsoe-Mall</div>
           <div className="lg:flex hidden w-full max-w-[500px]">
             <input
               className="border-2 border-accent px-6 py-2 w-full"
